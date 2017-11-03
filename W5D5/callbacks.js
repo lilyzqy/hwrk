961e68fc30a5edc8700e2aa0bbb6e8d00f2202aa
Lily Yang <zq.yang.lily@gmail.com>
2017-11-03 08:05:42 -0700

    phase3 done

--- W5D5/callbacks.js
+++ W5D5/callbacks.js
@@ -1,10 +1,35 @@
-window.setTimeout(function () {
-  alert('HAMMERTIME');
-}, 5000);
+// window.setTimeout(function () {
+//   alert('HAMMERTIME');
+// }, 5000);
+//
+//
+// function hammerTime(time){
+//   window.setTimeout(function () {
+//     alert(`${time} is hammertime!`)
+//   }, 5000);
+// }
 
+//tea?
+const readline = require('readline');
 
-function hammerTime(time){
-  window.setTimeout(function () {
-    alert(`${time} is hammertime!`)
-  }, 5000);
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuits(){
+  let first, second;
+
+  reader.question("Would you like some tea?",(response) =>{
+    console.log(`You replied ${response}`);
+
+    reader.question("Would you like some biscuits?",(response) =>{
+      console.log(`You replied ${response}`);
+
+      const firstRes = (first === "yes") ? "do" : "don't";
+      const secondRes = (second === "yes") ? "do" : "don't";
+      console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+      reader.close();
+    });
+  });
 }
