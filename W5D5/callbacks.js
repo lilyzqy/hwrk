b027e024aa16b85201926dd4e1fbdd9680dc3dac
Lily Yang <zq.yang.lily@gmail.com>
2017-11-03 08:06:14 -0700

    phase4 adding cat and dog class

--- W5D5/callbacks.js
+++ W5D5/callbacks.js
@@ -33,3 +33,20 @@ function teaAndBiscuits(){
     });
   });
 }
+
+function Cat () {
+  this.name = 'Markov';
+  this.age = 3;
+}
+
+function Dog () {
+  this.name = 'Noodles';
+  this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
+};
+
+const Markov = new Cat ();
+const Noodles = new Dog ();
